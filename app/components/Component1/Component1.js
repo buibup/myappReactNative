7f75a72fa6f71761871417ad7f55358595438882
svhit <svhit@svhits-MacBook-Pro.local>
2017-09-12 23:00:22 +0700

    add component2

--- app/components/Component1/Component1.js
+++ app/components/Component1/Component1.js
@@ -3,19 +3,24 @@ import {AppRegistry, Text, View} from 'react-native';
 
 export default class Component1 extends Component{
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       name:'Bui',
-      showName: true
+      showName: true,
+      message: this.props.message
     }
+  }
+
+  static defaultProps = {
+    message: 'Hi There'
   }
 
  render(){
    let name = this.state.showName ? this.state.name : 'No name';
    return(
       <View>
-       <Text>{this.props.message}</Text>
+       <Text>{this.state.message}</Text>
        <Text>{name}</Text>
       </View>
    );
